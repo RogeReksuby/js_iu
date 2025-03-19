762f38a2ec7058321a704e17b56d481a42cfe2e6
RogeReksuby <e.kulygin2018@gmail.com>
2025-03-20 01:41:15 +0300

    js scripts

--- script.js
+++ script.js
@@ -51,6 +51,8 @@ window.onload = function(){
         if (a === '') return
         selectedOperation = '/'
     }
+
+
     
     // кнопка очищения
     document.getElementById("btn_op_clear").onclick = function() { 
@@ -61,11 +63,171 @@ window.onload = function(){
         outputElement.innerHTML = 0
     }
     
+    document.getElementById("btn_op_percent").onclick = function() {
+        if (a == '') 
+        {
+            return;
+        }
+        expressionResult = (+a) / 100;
+        a = expressionResult.toString();
+        b = '';
+        selectedOperation = null;
+        outputElement.innerHTML = a;
+    }
+    
+    /*
+    document.getElementById("btn_on_sign").onclick = function() {
+        if (a == '')
+        {
+            return;
+        }
+
+        expressionResult = 0 - (+a);
+        a = expressionResult.toString();
+        b = '';
+        selectedOperation = null;
+        outputElement.innerHTML = a;
+    }
+    */
+
+    document.getElementById("btn_op_del").onclick = function() {
+        a = a.toString()
+        b = b.toString()
+        
+        if (a != '' && b == '')
+        {
+            if (a.length == 1 || (a.length == 2 && a[0] == '-'))
+            {
+                a = ''
+                outputElement.innerHTML = 0
+            }
+            else
+            {
+                a = a.substring(0, a.length - 1)
+                outputElement.innerHTML = a
+            }
+        }
+        else if (b != '')
+        {
+            if (b.length == 1 || (b.length == 2 && b[0] == '-'))
+                {
+                    b = ''
+                    outputElement.innerHTML = 0
+                }
+                else
+                {
+                    b = b.substring(0, b.length - 1)
+                    outputElement.innerHTML = b
+                }
+        }
+    }
+
+    document.getElementById("btn_op_sqrt").onclick = function() {
+        if (a != '' && b == '')
+        {
+            if (a > 0)
+            {
+                a = Math.sqrt(a)
+                outputElement.innerHTML = a
+            }
+            else
+            {
+                a = ''
+                outputElement.innerHTML = 'Error'
+            }
+        }
+        else if (b != '')
+        {
+            if (b > 0)
+                {
+                    b = Math.sqrt(b)
+                    outputElement.innerHTML = b
+                }
+                else
+                {
+                    b = ''
+                    outputElement.innerHTML = 'Error'
+                }
+        }
+    }
+
+
+
+    document.getElementById("btn_op_power_two").onclick = function() 
+    {
+        if (a != '' && b == '')
+            {
+                a = (+a) * (+a)
+                outputElement.innerHTML = a
+            }
+            else if (b != '')
+            {
+                b = (+b) * (+b)
+                outputElement.innerHTML = b
+            }
+    }
+
+    function fact(x)
+    {
+        if (x == 0 || x == 1) 
+        {
+            return 1
+        }
+        else
+        {
+            return x * fact(x - 1)
+        }
+    }
+
+    document.getElementById("btn_op_fct").onclick = function()
+    {
+        if (a != '' && b == '')
+        {
+            if (a % 1 == 0 && a > 0)
+            {
+                a = fact(a)
+                outputElement.innerHTML = a
+            }
+            else
+            {
+                a = ''
+                outputElement.innerHTML = 'Error'
+            }
+        }
+        else if (b != '')
+        {
+            if (b % 1 == 0 && b > 0)
+            {
+                b = fact(b)
+                outputElement.innerHTML = b
+            }
+            else
+            {
+                b = ''
+                outputElement.innerHTML = 'Error'
+            }
+        }
+    }
+
+    // операция смены знака +/-
+    document.getElementById("btn_op_sign").onclick = function() 
+    {
+        if (a != '' && b == '') {
+            a = ((-1) * a)
+            outputElement.innerHTML = a
+        } else if (b != '') {
+            b = ((-1) * b)
+            outputElement.innerHTML = b
+        } else return
+    }
+
+
+
     // кнопка расчёта результата
     document.getElementById("btn_op_equal").onclick = function() { 
         if (a === '' || b === '' || !selectedOperation)
             return
-            
+        console.log(a, ' ', b, ' ', selectedOperation);    
         switch(selectedOperation) { 
             case 'x':
                 expressionResult = (+a) * (+b)
