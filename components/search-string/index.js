6392484bdff345e41de8a88b17aec1ba077fda6d
RogeReksuby <165121967+RogeReksuby@users.noreply.github.com>
2025-05-28 11:43:41 +0300

    search

---
+++ components/search-string/index.js
@@ -0,0 +1,57 @@
+export class SearchStringComponent {
+    constructor(parent, value) {
+        this.parent = parent;
+        this.searchValue = ""
+        if (value !== null)
+        {
+            this.searchValue = value
+        }
+
+    }
+
+    searchStringListener(e)
+    {
+        this.searchValue = e.target.value
+        console.log(this.searchValue)
+    }
+
+    addListeners(searchListener) 
+    {
+        const inputElement = document.getElementById("search-string")
+        inputElement.addEventListener("input", (e) => {this.searchStringListener(e);})
+
+        const buttonElement = document.getElementById("filter-button")
+        buttonElement.addEventListener("click", searchListener)
+    }
+
+    getHTML() {
+        return (
+            `
+            <div>
+                <input type="text" id="search-string" placeholder="Введите название" />
+                <button id="filter-button" class="btn mycard-button" type="button">Поиск</button>
+            </div>
+            `
+        )
+    }
+
+    getValue()
+    {
+        return (this.searchValue)
+    }
+
+    render(onInput) {
+        const html = this.getHTML()
+        this.parent.insertAdjacentHTML('beforeend', html)
+
+        const buttonElement = document.getElementById("filter-button")
+        buttonElement.addEventListener("click", onInput)
+
+        //this.addListeners(onInput)
+        //const inputElement = document.getElementById("search-string");
+        //if (inputElement && onInput) {
+            //inputElement.addEventListener("input", onInput);
+        //}
+    
+    }
+}
